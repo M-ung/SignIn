e256804cdd32d67b540b237a1adb4be65744b544
kimjungmok <wjdahrrla@gmail.com>
2023-10-11 23:16:09 +0900

    first commit

---
+++ src/main/java/com/example/member/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.example.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberApplication.class, args);
+	}
+
+}
